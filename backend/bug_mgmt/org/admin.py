8e0356f350ef144e3cbf1da9a36b530526d141ea
Bastien Abadie <bastien@nextcairn.com>
2018-12-03 15:57:08 +0100

    Add Bugzilla components with owners

--- backend/bug_mgmt/org/admin.py
+++ backend/bug_mgmt/org/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from bug_mgmt.org.models import Person
+from bug_mgmt.org.models import Person, Category, Component
 
 
 class PersonAdmin(admin.ModelAdmin):
@@ -7,4 +7,15 @@ class PersonAdmin(admin.ModelAdmin):
     list_filter = ('manager', )
 
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+
+
+class ComponentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'owner')
+    list_filter = ('category', )
+
+
 admin.site.register(Person, PersonAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Component, ComponentAdmin)
