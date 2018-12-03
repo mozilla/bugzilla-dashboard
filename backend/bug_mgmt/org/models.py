b1bae89615722371573ea598506d74bb6e74b244
Bastien Abadie <bastien@nextcairn.com>
2018-12-03 15:35:37 +0100

    Load people from scrapped data

--- backend/bug_mgmt/org/models.py
+++ backend/bug_mgmt/org/models.py
@@ -5,8 +5,8 @@ class Person(models.Model):
     '''
     Someone in the Mozilla organisation
     '''
-    email = models.EmailField()
-    bugzilla_email = models.EmailField()
+    email = models.EmailField(unique=True)
+    bugzilla_email = models.EmailField(null=True, blank=True)
     name = models.CharField(max_length=250)
     title = models.CharField(max_length=250, null=True, blank=True)
 
@@ -17,3 +17,9 @@ class Person(models.Model):
         related_name='directs',
         on_delete=models.SET_NULL,
     )
+
+    class Meta:
+        ordering = ('name', )
+
+    def __str__(self):
+        return self.name
