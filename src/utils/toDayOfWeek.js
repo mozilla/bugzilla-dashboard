10e90f0f73a3f88ccdb0080b79414fc0baf2c50b
Armen Zambrano G <armenzg@gmail.com>
2019-01-07 15:42:40 -0500

    Bucket counting of bugs per week instead of per day

---
+++ src/utils/toDayOfWeek.js
@@ -0,0 +1,15 @@
+import moment from 'moment';
+
+// By default it changes the day to Friday
+// We're interested to know the state of bucket by the end of the week
+const toDayOfWeek = (dt = new Date(), dayOfWeek = 5) => {
+  let increment = 0;
+  // isoWeekDay represents Sunday as 7 instead of 0
+  const day = moment(dt).isoWeekday();
+  if (day > dayOfWeek) {
+    increment = 7;
+  }
+  return moment(dt).add(increment, 'days').isoWeekday(dayOfWeek).format('YYYY-MM-DD');
+};
+
+export default toDayOfWeek;
