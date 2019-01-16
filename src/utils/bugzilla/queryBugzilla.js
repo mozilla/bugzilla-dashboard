facff1c1e5fc62d6f0f95ec7585f645d72da5028
Armen Zambrano G <armenzg@gmail.com>
2019-01-16 12:13:22 -0500

    Make some Bugzilla fetches faster with default include_fields values

--- src/utils/bugzilla/queryBugzilla.js
+++ src/utils/bugzilla/queryBugzilla.js
@@ -16,6 +16,12 @@ const advancedSearchToRestApi = parameters => (
 );
 
 const generateBugzillaRestApiUrl = (queryParameters) => {
+  // include_fields is a Bugzilla trick to be specific about which properties to be
+  // returned from the Bugzilla REST APIs. This makes the fetches much faster
+  if (!queryParameters.include_fields) {
+    // eslint-disable-next-line no-param-reassign
+    queryParameters.include_fields = ['cf_last_resolved', 'creation_time'];
+  }
   const transformedParameters = advancedSearchToRestApi(queryParameters);
   const query = stringify({ ...transformedParameters });
   return `${settings.BZ_HOST}/rest/bug?${query}`;
