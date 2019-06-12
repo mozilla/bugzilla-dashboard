39ada188a7368143a48c659d61e405d6ee1a6b78
Shilpa Gowda <shilpa@exaltcg.com>
2019-06-12 19:15:45 +0530

    Feature - Replace tables with a more advanced component
    - Replaced Table in Reportees table with mui-datatable package
    - Removed Sorting function in Reportees component and using default mui-datatble sorting feature

--- src/components/Reportees/index.jsx
+++ src/components/Reportees/index.jsx
@@ -1,92 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import TableSortLabel from '@material-ui/core/TableSortLabel';
-import Tooltip from '@material-ui/core/Tooltip';
+import { withStyles, createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import MUIDataTable from 'mui-datatables';
 import CONFIG from '../../config';
 
 const styles = {
   root: {},
 };
 
-
-function propertyComparatorAscending(a, b, orderBy) {
-  const valueA = a[orderBy];
-  const valueB = b[orderBy];
-
-  // Values can either be strings or objects with a "count" property.
-  // Case 1: they're strings.
-  if (typeof valueA === 'string') {
-    return valueA.localeCompare(valueB);
-  }
-
-  // Case 2: they're objects with a count property.
-  const countA = valueA ? valueA.count : 0;
-  const countB = valueB ? valueB.count : 0;
-
-  return countA - countB;
-}
-
-function stableSort(array, cmp) {
-  const stabilizedThis = array.map((el, index) => [el, index]);
-  stabilizedThis.sort((a, b) => {
-    const order = cmp(a[0], b[0]);
-    if (order !== 0) return order;
-    return a[1] - b[1];
-  });
-  const result = stabilizedThis.map(el => el[0]);
-  return result;
-}
-
-function getSorting(order, orderBy) {
-  return order === 'asc'
-    ? (a, b) => propertyComparatorAscending(a, b, orderBy)
-    : (a, b) => -propertyComparatorAscending(a, b, orderBy);
-}
-
-function TableHeadCell({
-  metricUid, orderBy, order, label, onClick,
-}) {
-  return (
-    <TableCell
-      align="right"
-      sortDirection={orderBy === metricUid ? order : false}
-    >
-      <Tooltip
-        title="Click to sort the table by this column"
-        placement="bottom-end"
-      >
-        <TableSortLabel
-          active={orderBy === metricUid}
-          direction={order}
-          onClick={onClick}
-        >
-          {label}
-        </TableSortLabel>
-      </Tooltip>
-    </TableCell>
-  );
-}
-
-TableHeadCell.propTypes = {
-  metricUid: PropTypes.string.isRequired,
-  orderBy: PropTypes.string.isRequired,
-  order: PropTypes.string.isRequired,
-  label: PropTypes.string.isRequired,
-  onClick: PropTypes.func.isRequired,
-};
-
 class Reportees extends React.PureComponent {
-  state = {
-    orderBy: 'cn',
-    order: 'asc',
-  };
-
   getMergedProps() {
     const { metrics, partialOrg, ldapEmail } = this.props;
 
@@ -99,111 +21,94 @@ class Reportees extends React.PureComponent {
       ...reportee,
       ...metrics[reportee.bugzillaEmail],
     }));
-
-    return reporteesWithMetrics;
+    // Sort dataset in ascending order and return
+    return reporteesWithMetrics.sort((a, b) => a.cn.localeCompare(b.cn));
   }
 
-  createSortHandler = property => (event) => {
-    this.handleRequestSort(event, property);
-  };
-
-  handleRequestSort = (event, property) => {
-    const { order, orderBy } = this.state;
-
-    let newOrder;
-
-    // If we click on the same column several times in a row, we simply switch
-    // the prevous value.
-    if (orderBy === property) {
-      newOrder = order === 'asc' ? 'desc' : 'asc';
-    } else if (property === 'cn') {
-      // For most properties we want to order in the descending order first.
-      // ... except when sorting by cn.
-      newOrder = 'asc';
-    } else {
-      newOrder = 'desc';
-    }
-
-    this.setState({
-      orderBy: property,
-      order: newOrder,
-    });
-  };
+  // Custom styles to override default MUI theme
+  getMuiTheme = () => createMuiTheme({
+    overrides: {
+      MUIDataTableBodyCell: {
+        root: {
+          textAlign: 'center',
+        },
+      },
+    },
+  });
 
   render() {
     const { classes } = this.props;
 
-    const { order, orderBy } = this.state;
+    // MUI table options
+    const options = {
+      filter: true,
+      selectableRows: false,
+      sort: true,
+      responsive: 'stacked',
+      rowsPerPage: 10,
+      download: false,
+      print: false,
+      viewColumns: false,
+    };
+
     const metricsAsArray = Object.entries(CONFIG.reporteesMetrics);
+
+    const tableHeader = [];
+
+    // Form Table column headers using metricsArray
+    // Add Full name directly into columns Heading array
+    const firstColumn = {
+      name: 'cn',
+      label: 'Full Name',
+    };
+
+    tableHeader.push(firstColumn);
+
+    // push other columns from metricsAsArray in the config.js file to tableHeader array
+    metricsAsArray.forEach(([metricUid, { label }]) => {
+      const column = {
+        name: `${metricUid}`,
+        label,
+        options: {
+          filter: false,
+          customBodyRender: value => (
+            <a
+              href={value !== undefined ? value.link : '#'}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              { value !== undefined ? value.count : '' }
+            </a>
+          ),
+        },
+      };
+      tableHeader.push(column);
+    });
 
     return (
       <div className={classes.root}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableHeadCell
-                metricUid="cn"
-                orderBy={orderBy}
-                order={order}
-                label="Full Name"
-                onClick={this.createSortHandler('cn')}
-              />
-              {metricsAsArray.map(([metricUid, { label }]) => (
-                <TableHeadCell
-                  key={metricUid}
-                  metricUid={metricUid}
-                  orderBy={orderBy}
-                  order={order}
-                  label={label}
-                  onClick={this.createSortHandler(metricUid)}
-                />
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {stableSort(
-              this.getMergedProps(),
-              getSorting(order, orderBy),
-            )
-              .map(({
-                cn, mail, bugzillaEmail, ...metricsValues
-              }) => (
-                <TableRow key={mail}>
-                  <TableCell key={mail}>{cn}</TableCell>
-                  {metricsAsArray.map(([metricUid]) => {
-                    const countLink = metricsValues[metricUid];
-                    return (
-                      <TableCell align="right" key={metricUid}>
-                        {countLink && (
-                        <a
-                          key={countLink.link}
-                          href={countLink.link}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          {countLink.count}
-                        </a>
-                        )}
-                      </TableCell>
-                    );
-                  })}
-                </TableRow>
-              ))}
-          </TableBody>
-        </Table>
+        <MuiThemeProvider theme={this.getMuiTheme()}>
+          <MUIDataTable
+            title="Reportees"
+            data={this.getMergedProps()}
+            columns={tableHeader}
+            options={options}
+          />
+        </MuiThemeProvider>
       </div>
     );
   }
 }
 Reportees.propTypes = {
   classes: PropTypes.shape({}).isRequired,
-  ldapEmail: PropTypes.string.isRequired,
+  ldapEmail: PropTypes.string,
   partialOrg: PropTypes.shape({}).isRequired,
   metrics: PropTypes.shape({}),
 };
 
 Reportees.defaultProps = {
   metrics: {},
+  ldapEmail: '',
 };
 
 export default withStyles(styles)(Reportees);
