69aae56f5bf4329c4172739b545c22ab3b48fb94
Bastien Abadie <bastien@nextcairn.com>
2020-01-21 10:57:41 +0100

    List all reportees from a manager

--- src/utils/getAllReportees.js
+++ src/utils/getAllReportees.js
@@ -2,39 +2,29 @@ import pako from 'pako';
 import config from '../config';
 import loadArtifact from './artifacts';
 
-const findReportees = (org, userId) => {
-  console.log('Find all reportees of', userId);
+const findReportees = (org, parentId) => {
+  // Find all direct reportees of specified manager
   const managerFilter = (acc, key) => {
-    if (org[key].manager !== userId) {
+    if (org[key].manager !== parentId) {
       return acc;
     }
     return { ...acc, [key]: org[key] };
   };
-  return Object.keys(org).reduce(managerFilter, {});
-/*
-  let allReportees = {};
-  // if non-LDAP user, replace user email by the last email in list. Last email
-  const allEmails = Object.keys(completeOrg);
-  const checkedEmail = (email in completeOrg) ? email : allEmails[allEmails.length - 1];
+  let reportees = Object.keys(org).reduce(managerFilter, {});
 
-  allReportees[email] = completeOrg[checkedEmail];
-  const { reportees } = completeOrg[checkedEmail];
-
-
+  // Add subordinates reportees too
   if (reportees.length !== 0) {
-    reportees.forEach((reporteeEmail) => {
-      const partialOrg = findReportees(completeOrg, reporteeEmail);
-      allReportees = { ...allReportees, ...partialOrg };
+    Object.keys(reportees).forEach((rId) => {
+      const subordinates = findReportees(org, rId);
+      reportees = { ...reportees, ...subordinates };
     });
   }
-  return allReportees;
-*/
+  return reportees;
 };
 
 const getAllReportees = async (userSession, userId) => {
   const peopleGZ = await loadArtifact(userSession, config.artifactRoute, config.peopleTree);
   const people = JSON.parse(pako.inflate(peopleGZ, { to: 'string' }));
-
   return findReportees(people, userId || userSession.userId);
 };
 
